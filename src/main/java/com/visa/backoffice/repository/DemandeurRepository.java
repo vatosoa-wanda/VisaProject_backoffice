fc2869cdaa37c87bd6ac897e1090c98809e2474d
vatosoa-wanda <154837268+vatosoa-wanda@users.noreply.github.com>
2026-04-20 21:37:35 +0300

    repository dev 1 sprint 1
    repository:
    - demandeur
    - passeport

--- src/main/java/com/visa/backoffice/repository/DemandeurRepository.java
+++ src/main/java/com/visa/backoffice/repository/DemandeurRepository.java
@@ -4,6 +4,10 @@ import com.visa.backoffice.model.Demandeur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface DemandeurRepository extends JpaRepository<Demandeur, Long> {
+    Optional<Demandeur> findByNomAndDateNaissance(String nom, LocalDate dateNaissance);
 }
