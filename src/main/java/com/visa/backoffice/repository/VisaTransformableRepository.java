90ec227fb52144e6ce1b8d2d6ab68e0ec1d0c598
vatosoa-wanda <154837268+vatosoa-wanda@users.noreply.github.com>
2026-04-20 21:40:35 +0300

    repository dev 2 sprint 1
    repository :
    - piece
    - statuDemande
    - typeDemande
    - VisaTransformable

--- src/main/java/com/visa/backoffice/repository/VisaTransformableRepository.java
+++ src/main/java/com/visa/backoffice/repository/VisaTransformableRepository.java
@@ -4,6 +4,9 @@ import com.visa.backoffice.model.VisaTransformable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VisaTransformableRepository extends JpaRepository<VisaTransformable, Long> {
+    Optional<VisaTransformable> findByReferenceVisa(String referenceVisa);
 }
